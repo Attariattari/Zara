645581bcd6abb2c7007333917c1fa5fc2361e85d
Attariattari <attariattari549@gmail.com>
2025-02-03 18:21:04 +0500

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Default export
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 });
