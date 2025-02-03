499b5a92e5e9f23e32b5d02f4bc90e697c48c675
Attariattari <attariattari549@gmail.com>
2025-02-03 18:08:23 +0500

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "THIS_IS_UNDEFINED") return;
-        warn(warning);
-      },
-    },
-  },
 });
